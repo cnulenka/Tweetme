8a1b940f07d75ba0008c23ba56e004354be9d60b
cnulenka <sinulenka001@gmail.com>
2021-05-29 18:43:22 +0530

    added list,detail tweets view

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from tweets import models
 
 # Register your models here.
+admin.site.register(models.Tweet)
--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,43 @@
+import random
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
+from .models import Tweet
+
 # Create your views here.
+def home_view(request, *args, **kwargs):
+    return render(request, "pages/home.html", context={}, status=200)
+
+
+def tweet_list_view(request, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
+
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    """
+    REST API VIEW
+    Consume by JavaScript or Swift/Java/iOS/Andriod
+    return json data
+    """
+    data = {
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Not found"
+        status = 404
+    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
