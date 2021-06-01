6bc1397c09fda737e9c049f638b1f6883a51abf5
cnulenka <sinulenka001@gmail.com>
2021-06-01 20:37:05 +0530

    Tweet Action btn, limit list view by user
    1. Improve retweet rendering
    2. Modularise react components

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,18 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {TweetsComponent} from './tweets'
+import {TweetsComponent, TweetDetailComponent} from './tweets'
 import * as serviceWorker from './serviceWorker';
 
 const appEl = document.getElementById('root')
 if (appEl) {
     ReactDOM.render(<App />, appEl);
 }
+const e = React.createElement
 const tweetsEl = document.getElementById("tweetme-2")
 if (tweetsEl) {
-    ReactDOM.render(<TweetsComponent />, tweetsEl);
+    ReactDOM.render(
+        e(TweetsComponent, tweetsEl.dataset), tweetsEl);
 }
 
+const tweetDetailElements = document.querySelectorAll(".tweetme-2-detail")
+
+tweetDetailElements.forEach(container=> {
+    ReactDOM.render(
+        e(TweetDetailComponent, container.dataset), 
+        container);
+})
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- frontend/src/tweets/index.js
+++ frontend/src/tweets/index.js
@@ -1,8 +1,17 @@
-import {ActionBtn, Tweet, TweetsList, TweetsComponent} from './components'
+import {ActionBtn} from './buttons'
+
+import {TweetsComponent, TweetDetailComponent} from './components'
+import {TweetCreate} from './create'
+import {Tweet} from './detail'
+import {TweetsList} from './list'
+
 
 export {
     ActionBtn, 
     Tweet,
+    TweetCreate,
+    TweetsComponent,
+    TweetDetailComponent,
     TweetsList,
-    TweetsComponent
+
 }
